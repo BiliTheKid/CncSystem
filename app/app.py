a087c06070af913c42eaa1fc95a8cb36b843a793
BiliTheKid <hazanyarden@icloud.com>
2020-04-04 15:08:41 +0300

    heroku info

--- app/app.py
+++ app/app.py
@@ -56,6 +56,9 @@ def GetMedicalCentersNumOfPatients(medicalcenter_id , department_id , severity):
 
 
 
+
+
+
 # start the server with the 'run()' method
 if __name__ == '__main__':
     app.run(debug=True)
